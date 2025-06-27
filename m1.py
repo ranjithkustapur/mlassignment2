a600cbc962f1fc12a7c2c8d97033863afafb165b
ranjithkustapur <122463901+ranjithkustapur@users.noreply.github.com>
2025-06-27 12:34:30 -0500

    changed script

--- m1.py
+++ m1.py
@@ -1,5 +1,5 @@
 #QUESTION1
-rows = 5
+rows = 4
 for i in range(0, rows):
     for j in range(0, i + 1):
         print("*", end=' ')
@@ -7,5 +7,11 @@ for i in range(0, rows):
 
 for i in range(rows, 0, -1):
     for j in range(0, i - 1):
+        print("*", end=' ')
+    print("\r")
+    
+    
+for i in range(rows, 0, -1):
+    for j in range(0, i + 1):
         print("*", end=' ')
     print("\r")
